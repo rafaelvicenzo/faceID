c75c3176f846cef0a4ad713f5f6ddcf5c7c3b1bf
rafaelvicenzo <rafaellordes.lordes@gmail.com>
2024-08-27 14:30:46 -0300

    Cam implementation

--- src/scripts/js/script.js
+++ src/scripts/js/script.js
@@ -1,16 +1,18 @@
 const video = document.getElementById('video')
 
-Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUrl('../models'),
-    faceapi.nets.faceLandmark68Net.loadFromUrl('../models'),
-    faceapi.nets.faceRecognitionNet.loadFromUrl('../models'),
-    faceapi.nets.faceExpressionNet.loadFromUrl('../models')
-]).then(startVideo)
+async function startVideo() {
+    constraints = {video: true}
 
-function startVideo() {
-    navigator.getUserMedia(
-        {video: {}},
-        stream => video.srcObject = stream,
-        err => console.error(err)
-    )
+    try {
+        let stream = await navigator.mediaDevices.getUserMedia(constraints)
+
+        cam.srcObject = stream
+        cam.onloadedmetadata = () => {
+            cam.play()
+        }
+    } catch (error) {
+        console.log(error)
+    }
 }
+
+startVideo();
