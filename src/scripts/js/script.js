47ee1f556b435cd908333c81fa78c6e68cf70693
rafaelvicenzo <rafaellordes.lordes@gmail.com>
2024-08-27 21:29:35 -0300

    Face Recognition

--- src/scripts/js/script.js
+++ src/scripts/js/script.js
@@ -1,5 +1,12 @@
 const video = document.getElementById('video')
 
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('../models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('../models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('../models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('../models')
+]).then(startVideo)
+
 async function startVideo() {
     constraints = {video: true}
 
@@ -15,4 +22,26 @@ async function startVideo() {
     }
 }
 
-startVideo();
+video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+
+    const displaySize = {width: video.width, height: video.height}
+
+    faceapi.matchDimensions(canvas, displaySize)
+    setInterval(async() => {
+        const detections = await faceapi.detectAllFaces(
+            video,
+            new faceapi.TinyFaceDetectorOptions()
+        )
+        .withFaceLandmarks()
+
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+
+        canvas.getContext('2d').clearRect(0,0, canvas.width, canvas.height)
+
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+    }, 100)
+})
+
+/*TROCAR O DETECTALLFACES POR SINGLE FACES*/
